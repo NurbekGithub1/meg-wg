5fb4229381443be891c31478589ce837869bc023
kebrungem <nderbisali@gmail.com>
2025-08-28 14:48:18 +0500

    Поправки в MainWindow

--- src/AFWGSS.Main/MainWindow.xaml.cs
+++ src/AFWGSS.Main/MainWindow.xaml.cs
@@ -1,13 +1,19 @@
-﻿// src/AFWGSS.Main/MainWindow.xaml.cs
-using MahApps.Metro.Controls;
+﻿using MahApps.Metro.Controls;
+using System.Windows;
 
 namespace AFWGSS.Main
 {
     public partial class MainWindow : MetroWindow
     {
+        // Конструктор теперь пустой! ViewModel все сделает сам.
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MenuItem_Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
