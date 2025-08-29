1c42091a28228795e2209e24248c6f7d6fcf99be
kebrungem <nderbisali@gmail.com>
2025-08-29 18:05:48 +0500

    Исправление багов, доработка главного окна

--- src/AFWGSS.Main/ViewModels/MainWindowViewModel.cs
+++ src/AFWGSS.Main/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,8 @@
 // src/AFWGSS.Main/ViewModels/MainWindowViewModel.cs
-using Prism.Mvvm;
 using Prism.Commands;
-using Prism.Navigation.Regions;   // Prism 9
-using AFWGSS.Shared;
+using Prism.Mvvm;
+using Prism.Navigation.Regions;
+using System.Windows; // GridLength
 
 namespace AFWGSS.Main.ViewModels
 {
@@ -12,16 +12,51 @@ namespace AFWGSS.Main.ViewModels
 
         public DelegateCommand<string> NavigateCommand { get; }
 
+        // --- размеры панели ---
+        private GridLength _sideBarWidth = new GridLength(160);
+        public GridLength SideBarWidth
+        {
+            get => _sideBarWidth;
+            private set => SetProperty(ref _sideBarWidth, value);
+        }
+
+        private bool _isSidebarCollapsed;
+        public bool IsSidebarCollapsed
+        {
+            get => _isSidebarCollapsed;
+            set
+            {
+                if (SetProperty(ref _isSidebarCollapsed, value))
+                    SideBarWidth = value ? new GridLength(56) : new GridLength(160);
+            }
+        }
+
+        // Для подсветки активного пункта
+        private string _currentViewKey = string.Empty;
+        public string CurrentViewKey
+        {
+            get => _currentViewKey;
+            set => SetProperty(ref _currentViewKey, value);
+        }
+
         public MainWindowViewModel(IRegionManager regionManager)
         {
             _regionManager = regionManager;
-            NavigateCommand = new DelegateCommand<string>(ExecuteNavigate);
+
+            NavigateCommand = new DelegateCommand<string>(Navigate);
+
+            // начальное состояние панели
+            IsSidebarCollapsed = false;                      // раскрыта
+            SideBarWidth = new GridLength(160);
         }
 
-        private void ExecuteNavigate(string viewKey)
+        private void Navigate(string viewKey)
         {
-            if (string.IsNullOrWhiteSpace(viewKey)) return;
-            _regionManager.RequestNavigate(RegionNames.ContentRegion, viewKey);
+            if (string.IsNullOrWhiteSpace(viewKey))
+                return;
+
+            _regionManager.RequestNavigate(AFWGSS.Shared.RegionNames.ContentRegion, viewKey);
+            CurrentViewKey = viewKey;
         }
     }
 }
